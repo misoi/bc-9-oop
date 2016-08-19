4efa1aad719a8d56bb197f4d49f81ec8198ab237
misoi <cytnhia.chepkemoi@gmail.com>
2016-08-19 05:58:51 +0300

    file which has the names of the students and its suppose to print those who attend class on a specific date

---
+++ student_attend.py
@@ -0,0 +1,14 @@
+from student import Student
+
+
+s1 = Student('adidas', 'mbalanya', 'UG')
+s2 = Student('cynthia', 'chepkemoi')
+s3 = Student('victor', 'muthomi', 'TZ')
+s4 = Student('Kennedy', 'mutemi')
+s5 = Student('paul', 'kahohi')
+
+s1.attend_class(s1 = fname, date = date)
+
+print ('kennedy'...)
+
+
